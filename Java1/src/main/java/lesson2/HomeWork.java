d73001483d66cc4ee415a1718d151f018aacfcb6
AntonKravchenko <kravchenko_ae@mail.ru>
2020-10-19 12:02:38 +0300

    Исправления в последней задаче

--- Java1/src/main/java/lesson2/HomeWork.java
+++ Java1/src/main/java/lesson2/HomeWork.java
@@ -137,7 +137,7 @@ public class HomeWork {
         int bucket;     //переменная для хранения изменяемого элемента массива
         int curCounter = 0;
 
-        while (curCounter != n) {
+        while (curCounter != n % size) {
 
             if (n < 0) {
                 //сдвиг влево
